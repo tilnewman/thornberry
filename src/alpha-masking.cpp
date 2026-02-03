3d0e1a0801b42c9ef766da14b37e43369af30cc6
Ziesche Til Newman <tilnewman@protonmail.com>
2026-02-03 03:26:10 -0700

    adding some needed comments

--- src/alpha-masking.cpp
+++ src/alpha-masking.cpp
@@ -44,19 +44,23 @@ namespace thornberry
                 {
                     if (color == sf::Color::Black)
                     {
+                        // black is the darkest shadow color
                         t_image.setPixel(pixelPos, sf::Color(0, 0, 0, 124));
                     }
                     else if (color == sf::Color(127, 0, 127))
                     {
+                        // this half-magenta color is the medium shadow
                         t_image.setPixel(pixelPos, sf::Color(0, 0, 0, 87));
                     }
                     else if (color == sf::Color(151, 0, 147))
                     {
-                        // this one shouldn't be needed but some shadow images use it so...
+                        // This half-magenta variant shouldn't be needed, but
+                        // until I fix all the shadow images out there that use it...
                         t_image.setPixel(pixelPos, sf::Color(0, 0, 0, 87));
                     }
                     else if (color == sf::Color(255, 0, 255))
                     {
+                        // the magenta color is the lightest shadow color
                         t_image.setPixel(pixelPos, sf::Color(0, 0, 0, 50));
                     }
                 }
