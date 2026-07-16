6d606b055ac45b54afe7efb16607be832c7bb093
Ziesche Til Newman <tilnewman@protonmail.com>
2026-07-16 07:13:33 -0600

    fix a call order issue

--- src/coordinator.cpp
+++ src/coordinator.cpp
@@ -196,9 +196,9 @@ namespace thornberry
         // update the level before player and npcs
         m_levelUPtr->update(*m_contextUPtr, t_elapsedSec);
         m_playerUPtr->update(*m_contextUPtr, t_elapsedSec);
+        m_npcManagerUPtr->update(*m_contextUPtr, t_elapsedSec);
 
         // this order is NOT critical
-        m_npcManagerUPtr->update(*m_contextUPtr, t_elapsedSec);
         m_smokeParticleEffectsUPtr->update(*m_contextUPtr, t_elapsedSec);
         m_predrawAnimationsUPtr->update(*m_contextUPtr, t_elapsedSec);
         m_dayNightCycleUPtr->update(*m_contextUPtr, t_elapsedSec);
